7522561e68d6a98d68dd329a2edfe1db98d0c18d
Justin <jhud1998@gmail.com>
2023-07-01 19:58:15 -0500

    Commented Code

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
+// Links to connect
 const express = require('express');
 const routesApi = require('./routes/API');
 const routesHtml = require('./routes/HTML');
 
+// PORT and Express call
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+// Middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static('public'));
 app.use(routesApi);
 app.use(routesHtml);
 
+// Establishes the PORT
 app.listen(PORT, () => {
     console.log(`Listening localhost:${PORT}`);
 })
